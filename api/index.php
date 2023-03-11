225139dca67271e6d6be5e2cca26bc19ae22bad2
Rafael Mattos <rhafu29@gmail.com>
2023-03-11 18:17:07 -0300

    Novas Query Strings e Verificações

--- api/index.php
+++ api/index.php
@@ -13,7 +13,24 @@ if(isset($_GET['option'])){
             break;
 
         case 'random':
-            define_response($data, rand(0, 1000));
+            //Vou verificar se vem min ou max no GET
+            $min = 0;
+            $max = 1000;
+
+            if(isset($_GET['min'])){
+                $min = intval($_GET['min']);
+            }
+
+            if(isset($_GET['max'])){
+                $max = intval($_GET['max']);
+            }
+
+            if($min >= $max){
+                response($data);
+                return;
+            }
+
+            define_response($data, rand($min, $max));
             break;
 
     }
